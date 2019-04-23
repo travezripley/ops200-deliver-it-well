f877a9a27a987de6f503770f986e7b185ec40248
Travez Ripley <theripleys@gmail.com>
2019-04-23 13:26:28 -0700

    updates tests - tests pass

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,26 +1,26 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server/app');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server/app");
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 server.listen(4444);
 
-describe('server/app.js', function() {
+describe("server/app.js", function() {
   this.timeout(5000);
-  beforeEach((done) => {
-    
+  beforeEach(done => {
     done();
   });
 
-  afterEach((done) => {
-      done();
-  })
+  afterEach(done => {
+    done();
+  });
 
-  it('responds to /', (done) => {
-    chai.request(server)
-      .get('/')
+  it("responds to /", done => {
+    chai
+      .request(server)
+      .get("/")
       .end((err, res) => {
         expect(err).not.exist;
         expect(res).to.have.status(200);
@@ -28,13 +28,27 @@ describe('server/app.js', function() {
       });
   });
 
-  it('page says hello world', (done) => {
-  chai.request(server)
-    .get('/')
-    .end((err, res) => {
-      expect(err).not.exist;
-      expect(JSON.stringify(res.text)).to.contain('Hello World');
-      done();
-    });
+  it("page says hello world", done => {
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain("Hello World");
+        done();
+      });
   });
-})
+
+  it("page says Sup Dawg, UFO's are real! I want to believe.", done => {
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain(
+          "Sup Dawg, UFO's are real! I want to believe."
+        );
+        done();
+      });
+  });
+});
